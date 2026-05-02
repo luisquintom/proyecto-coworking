bc9e4a141f4552f0efe4e11181f1eb5fdd4ef3df
Luis Quinto <leqvmx@gmail.com>
2026-05-02 14:36:13 +0100

    Actualizacinado archivo urls.py

--- Back-end/config/urls.py
+++ Back-end/config/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/usuarios/', include('usuarios.urls')),
-    # LOGIN: Este endpoint pedirá Email y Password por la lógica anterior
+    # pedirá Email y Password en el login
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('coworking.urls')),
